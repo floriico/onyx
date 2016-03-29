2f82d5935143783846bf6c6c1fb359069438820e
floriico <floriico@gmail.com>
2016-03-29 21:15:35 +0200

    Sort entities by depth before display

--- js/game-renderer.js
+++ js/game-renderer.js
@@ -8,14 +8,14 @@ define([], function () {
   };
 
   GameRenderer.prototype.paint = function paint() {
-    var len = this.entities.length;
-    var i;
-    var entity;
-
     this.gc.fillStyle = '#002b36';
     this.gc.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    for (i = 0; i < len; i++) {
-      entity = this.entities[i];
+    const entities = this.entities.sort(function depthSort(entityA, entityB) {
+      return entityA.position.y - entityB.position.y;
+    })
+    const len =  entities.length;
+    for (let i = 0; i < len; i++) {
+      let entity = entities[i];
       this.gc.save();
       this.gc.translate(Math.round(entity.position.x), Math.round(entity.position.y));
       entity.componentGraphic.paint(this.gc);
